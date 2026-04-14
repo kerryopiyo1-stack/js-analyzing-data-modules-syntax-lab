8bfb719d88b0aa6d7e3a8ac3a32ec2a378558b55
kerry opiyo <kerryopiyo0758@gmail.com>
2026-04-14 14:13:34 +0300

    syntax modules

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
+require('datejs'); // Ensure datejs is available
+
+function mergeUsernames(...args) {
+    let allUsers = [];
+
+    for (const usersArray of args) {
+        allUsers = [...allUsers, ...usersArray];
+    }
+
+    return allUsers;
+}
+
+function combineUsers(...args) {
+    const combinedObject = {
+        users: []
+    };
+
+    for (const usersArray of args) {
+        combinedObject.users = [...combinedObject.users, ...usersArray];
+    }
+
+    // Use built-in Date to format as M/d/yyyy
+    combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+    return combinedObject;
+}
+
+// Example usage
+const group1 = ["kerry", "opiyo"];
+const group2 = ["ken", "duncan"];
+const group3 = ["eve"];
+
+console.log(mergeUsernames(group1, group2, group3));
+// ["kerry", "opiyo", "ken", "duncan", "eve"]
+
+console.log(combineUsers(group1, group2, group3));
+// {
+//   users: ["kerry", "opiyo", "ken", "duncan", "eve"],
+//   merge_date: "4/14/2026"
+// }
 
 
+
+    
+
+module.exports = { mergeUsernames, combineUsers };
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
